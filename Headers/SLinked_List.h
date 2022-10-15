5a8732d02a9869c1615608aeb5a76aecb361e7b5
Edwin Franco Sanchez <edfrancos@unal.edu.co>
2022-10-15 13:55:50 -0500

    Add some SLinked_list methods

--- Headers/SLinked_List.h
+++ Headers/SLinked_List.h
@@ -10,12 +10,15 @@ class SLinked_List{
       SNode<keyType>* HeadList;
       SNode<keyType>* TailList;
    public:
-      // Constructors
+   // Constructors
       SLinked_List(){
          HeadList = nullptr;
          TailList = nullptr;
       }
-      /*  Front Methods  */
+
+   /*  Front Methods  */
+
+      // Add a new element before the first list element.
       void pushFront(keyType key){
          SNode<keyType>* node = new SNode<keyType>(key);
          node->setTail(HeadList);
@@ -24,9 +27,10 @@ class SLinked_List{
             TailList = node;
          }
       }
+      // Remove the first list element.
       void popFront(){
          if(HeadList == nullptr){
-            std::cout<<"ERROR: Empty List"<<std::endl;
+            std::cout<<"ERROR: Empty List \n";
             return;
          }
          SNode<keyType>* currentHead = HeadList;
@@ -36,7 +40,14 @@ class SLinked_List{
             TailList = nullptr;
          }
       }
-      // Back Methods
+      // Get the key of the first list element.
+      keyType topFront(){
+         return HeadList->getKey();
+      }
+
+   /*  Back Methods  */
+
+      // Add a new element after the last list element.
       void pushBack(keyType key){
          SNode<keyType>* node = new SNode<keyType>(key);
          if(TailList == nullptr){
@@ -47,24 +58,71 @@ class SLinked_List{
          TailList->setTail(node);
          TailList = node;
       }
+      // Remove the last list element.
       void popBack(){
          if( HeadList == nullptr){
-            std::cout<<"ERROR: Empty List"<<std::endl;
+            std::cout<<"ERROR: Empty List \n";
             return;
          }
          if( HeadList == TailList){
             delete HeadList; HeadList = nullptr; TailList = nullptr;
             return;
          }
-         SNode<keyType>* currentNode=HeadList;
-         while(currentNode->getTail()->getTail() == nullptr){
+         SNode<keyType>* currentNode= HeadList;
+         while(currentNode->getTail()->getTail() != nullptr){
             currentNode = currentNode->getTail();
          }
          TailList = currentNode;
+         TailList->setTail(nullptr);
          currentNode = currentNode->getTail();
          delete currentNode; currentNode = nullptr;
       }
-      // Special Methods
+      // Get the key of the last list element.
+      keyType topBack(){
+         return TailList->getKey();
+      }
+
+   
+   /*  Special Methods  */
+
+      // Search a key in the list
+      bool find(keyType key){
+         SNode<keyType>* currentNode = HeadList;
+         while(currentNode->getKey() != key){
+            currentNode = currentNode->getTail();
+            if( currentNode == TailList && currentNode->getKey() != key){
+               return false;
+            }
+         }
+         return true;
+      }
+      // Remove the element that has a specific key
+      void erase(keyType key){
+         SNode<keyType>* currentNode = HeadList;
+         if(HeadList->getKey() == key){
+            popFront(); return;
+         }
+         while(currentNode->getTail()->getKey() != key){
+            currentNode = currentNode->getTail();
+            if(currentNode == TailList){
+               std::cout<<"ERROR: Not found Element \n";
+               return;
+            };
+         }
+         SNode<keyType>* deleteNode = currentNode->getTail();
+         currentNode->setTail(deleteNode->getTail());
+         if(TailList == deleteNode){
+            TailList = currentNode;
+         }
+         delete deleteNode; deleteNode = nullptr;
+      }
+      // Is the list empty?
+      bool empty(){
+         if(HeadList == TailList && HeadList==nullptr){
+            return true;
+         }
+         return false;
+      }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,11 +4,5 @@
 
 int main(){
    SLinked_List<int> list1 = SLinked_List<int>();
-   list1.pushBack(3);
-   list1.pushFront(2);
-   list1.pushBack(4);
-   list1.pushFront(1);
-   list1.pushBack(5);
-   list1.pushFront(0);
    return 0;
 }
